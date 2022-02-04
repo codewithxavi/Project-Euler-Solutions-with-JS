999364cb0042114b2502ddca0d735ac651679462
xavicoder <70905472+xavi-coder@users.noreply.github.com>
2022-02-04 22:12:40 +0100

    Update #1.js

--- #1.js
+++ #1.js
@@ -1,13 +1,13 @@
 function multiplesOf3and5(number) {
-      let total = 0;
-      for (let i = 0; i <= number; i++) {
-        let multipleOf3 = (i % 3 == 0)
-        let multipleOf5 = (i % 5 == 0)
-        if (multipleOf3 || multipleOf5) {
-          total += i;
-        }
-      }
-      return total;
+  let total = 0;
+  for (let i = 0; i <= number; i++) {
+    let multipleOf3 = i % 3 == 0;
+    let multipleOf5 = i % 5 == 0;
+    if (multipleOf3 || multipleOf5) {
+      total += i;
     }
+  }
+  return total;
+}
+multiplesOf3and5(10)multiplesOf3and5(10);
 
-multiplesOf3and5(10)
